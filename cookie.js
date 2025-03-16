73d9432cf8536443b7c4818ee3bb6858f359e606
Forge59 <borisorbodoev2007@gmail.com>
2025-03-16 11:08:31 +0400

    score

---
+++ cookie.js
@@ -0,0 +1,15 @@
+// import { Game } from './game.js';
+
+// export class BestScore{
+//     bestScore = 0;
+//     score = Game.score;
+
+// saveCookie(){
+//     if(this.score > this.bestScore){
+//         this.bestScore = this.getCookie('bestScore');
+//         console.log("score " + this.score);
+//         this.bestScore = this.score;
+//     }
+// }
+
+// }
